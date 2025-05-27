c58b798cd6e105c079161c062244be0db038e38f
gicorada <me@gicorada.com>
2025-05-27 11:52:56 +0200

    Quartz sync: May 27, 2025, 11:38 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "it-IT",
     baseUrl: "scuola.gicorada.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
